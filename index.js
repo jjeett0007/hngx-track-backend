61a93f9f867ddb89acc0ccdb73b15ddde3f5c63e
jet <88599825+jjeett0007@users.noreply.github.com>
2023-09-08 22:38:45 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const moment = require('moment');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api', (req, res) => {
   const { slack_name, track } = req.query;
